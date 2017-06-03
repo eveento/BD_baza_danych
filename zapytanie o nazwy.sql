115f964730a75af201515c8e41144c8f30c4fa38
tubidubidam <jacekzalewski95@gmail.com>
2017-06-03 02:54:41 +0200

    pseudokod

--- zapytanie o nazwy.sql
+++ zapytanie o nazwy.sql
@@ -2,7 +2,7 @@ SELECT Nazwa_dania
 FROM restauracja.dania 
 JOIN zamowienia_has_dania  
 ON Dania_ID_Dania = ID_Dania
-WHERE Zamowienia_ID_zamowienia = 2;
+WHERE Zamowienia_ID_zamowienia = 1;
 
 #SELECT Dania_ID_Dania 
 #FROM zamowienia_has_dania 
